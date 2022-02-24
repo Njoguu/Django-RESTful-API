19760e899ff3d5e4de5a74a9e7ed2fc28c84d501
Njoguu <njoguuallan@gmail.com>
2022-02-24 14:26:47 +0300

    Using Swagger UI

--- webapi/urls.py
+++ webapi/urls.py
@@ -17,8 +17,28 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from demo import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+...
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Demo Django RESTful API",
+      default_version='v1',
+      description="Test API",
+    #   terms_of_service="https://www.google.com/policies/terms/",
+    #   contact=openapi.Contact(email="bigboydevelops@gmail.com"),
+    #   license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('developers/', views.developerList.as_view())
+    path('developers/', views.developerList.as_view()),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
