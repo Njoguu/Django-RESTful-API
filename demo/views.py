7bfaab9ebc7311c703f85684aa724827f4fdec6f
Njoguu <njoguuallan@gmail.com>
2022-02-28 20:27:22 +0300

    User Account Activation

--- demo/views.py
+++ demo/views.py
@@ -12,6 +12,8 @@ from .models import User
 from .utils import Util
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
+import jwt
+from django.conf import settings
 
 # Create your views here.
 class RegisterView(generics.GenericAPIView):
@@ -45,6 +47,20 @@ class RegisterView(generics.GenericAPIView):
         return Response(user_data, status=status.HTTP_201_CREATED) # -->returns response that user has been created
 
 class VerifyEmail(generics.GenericAPIView):
-    def get(self):
-        pass 
+    def get(self, request):
+        token = request.GET.get('token') 
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+            user = User.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+
+            return Response({'email':'Successfully Activated!'}, status=status.HTTP_200_OK)
+
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error':'Activation link has expired!'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error':'Invalid token, please request a new one!'}, status=status.HTTP_400_BAD_REQUEST)
 
